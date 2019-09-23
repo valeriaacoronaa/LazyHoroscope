e622f4ed868f282e1924cf7e098ee25cacaf4c26
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-09-23 16:32:55 -0700

    tells user their lucky number

--- main.cpp
+++ main.cpp
@@ -9,13 +9,16 @@ int main()
   //declare variables
   string firstname= " ";
   string lastname= " ";
+  int lucky= 0;
   //get user input
   cout << "What is your first name?\n";
   cin  >> firstname;
   cout << "What is your last name?\n";
   cin  >> lastname;
   //tell fortune
-  cout << "Welcome, " << firstname.at(0) << '.' << lastname.at(0)  << ", here is your fortune...\n";
+  lucky = firstname.length();
+  cout << "Welcome, " << firstname.at(0) << '.' << lastname.at(0)  << ", here is your fortune...\n"
+       << "your lucky number is " << lucky << endl;
 
   return 0;
 }
