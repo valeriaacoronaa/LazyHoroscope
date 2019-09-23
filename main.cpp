39d5434ddb734e41555f7a762bce9d989065e690
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-09-23 16:25:50 -0700

    prints with user's nickname

--- main.cpp
+++ main.cpp
@@ -7,15 +7,15 @@ using namespace std;
 int main()
 {
   //declare variables
-  string firstname;
-  string lastname;
+  string firstname= " ";
+  string lastname= " ";
   //get user input
   cout << "What is your first name?\n";
   cin  >> firstname;
   cout << "What is your last name?\n";
   cin  >> lastname;
   //tell fortune
-  cout << "Welcome, " << firstname << ' ' << lastname  << ", here is your fortune...\n";
+  cout << "Welcome, " << firstname.at(0) << '.' << lastname.at(0)  << ", here is your fortune...\n";
 
   return 0;
 }
