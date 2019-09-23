8d979ed4940f149f9bdbb3ff3a6c27047ba232d0
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-09-23 16:18:04 -0700

    gets user's first and last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Valeria Corona
 #include<iostream>
 #include<string>
 
@@ -7,10 +7,15 @@ using namespace std;
 int main()
 {
   //declare variables
-
+  string firstname;
+  string lastname;
   //get user input
-
+  cout << "What is your first name?\n";
+  cin  >> firstname;
+  cout << "What is your last name?\n";
+  cin  >> lastname;
   //tell fortune
+  cout << "Welcome, " << firstname << ' ' << lastname  << ", here is your fortune...\n";
 
   return 0;
 }
